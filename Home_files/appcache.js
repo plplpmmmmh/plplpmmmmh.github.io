a9313822395fe8d9a09302ab9be2acf7c7902eda
plplpmmmmh <147246648+plplpmmmmh@users.noreply.github.com>
2023-10-08 09:22:32 +0800

    Delete Home_files directory

--- Home_files/appcache.js
+++
@@ -1,6 +0,0 @@
-window.applicationCache.addEventListener('updateready', function() {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-        window.applicationCache.swapCache();
-        window.location.reload();
-    }
-});
